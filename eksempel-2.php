0b849cd6963159e6d4b3dcd4d5b661a0db446b70
teoaanes <teoaanes@gmail.com>
2026-09-07 14:35:21 +0200

    lagt til matematikk eksempel 2

--- eksempel-2.php
+++ eksempel-2.php
@@ -2,5 +2,8 @@
 $number1 = $_POST["number1"];
 $number2 = $_POST["number2"];
 
-    echo "Summen av " . $number1 . " og " . $number2 . " er " . ($number1 + $number2) . "!" . "<br>" . "<button type='button' onclick='window.location.href=\"index.html\"'>Tilbake</button>";
+    echo "tallene dine er " . $number1 . " og " . $number2 . "<br>";
+    echo "Summen av " . $number1 . " og " . $number2 . " er " . ($number1 + $number2) . "!";
+    echo "Differansen mellom " . $number1 . " og " . $number2 . " er " . ($number1 - $number2) . "!";
+    echo "<br>" . "<button type='button' onclick='window.location.href=\"index.html\"'>Tilbake</button>";
 ?>
