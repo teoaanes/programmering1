564f2493f08084c149c2c333eb9c646922f3c9f8
teoaanes <teoaanes@gmail.com>
2026-09-07 14:43:53 +0200

    bedre formatering

--- eksempel-2.php
+++ eksempel-2.php
@@ -2,8 +2,8 @@
 $number1 = $_POST["number1"];
 $number2 = $_POST["number2"];
 
-    echo "tallene dine er " . $number1 . " og " . $number2 . "<br>";
-    echo "Summen av " . $number1 . " og " . $number2 . " er " . ($number1 + $number2) . "!" . "<br>";
-    echo "Differansen mellom " . $number1 . " og " . $number2 . " er " . ($number1 - $number2) . "!";
-    echo "<br>" . "<button type='button' onclick='window.location.href=\"index.html\"'>Tilbake</button>";
+    echo "Tallene dine er " . $number1 . " og " . $number2 . "<br>";
+    echo "Summen av " . $number1 . " og " . $number2 . " er " . ($number1 + $number2) . "<br>";
+    echo "Differansen mellom " . $number1 . " og " . $number2 . " er " . ($number1 - $number2) . "<br>";
+    echo "<button type='button' onclick='window.location.href=\"index.html\"'>Tilbake</button>";
 ?>
