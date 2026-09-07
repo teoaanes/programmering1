ce87eb98cd2977d0910d7e3765482d1d2dca5c84
teoaanes <teoaanes@gmail.com>
2026-09-07 15:15:50 +0200

    style på PHP output

--- eksempel-1/eksempel-1.php
+++ eksempel-1/eksempel-1.php
@@ -3,5 +3,5 @@ $fornavn = $_POST["fornavn"];
 $etternavn = $_POST["etternavn"];
 $alder = $_POST["alder"];
 
-    echo "God dag " . $fornavn . " " . $etternavn . "!" . "<br>" . "Du er " . $alder . " år gammel." . "<br>" . "<button type='button' onclick='window.location.href=\"index.html\"'>Tilbake</button>";
+    echo '<link rel="stylesheet" href="eksempel-1/style.css">' . "God dag " . $fornavn . " " . $etternavn . "!" . "<br>" . "Du er " . $alder . " år gammel." . "<br>" . "<button type='button' onclick='window.location.href=\"index.html\"'>Tilbake</button>";
 ?>
