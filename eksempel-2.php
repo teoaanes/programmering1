d18e738b26a6668066013bb8c93bee19a01e06ad
teoaanes <teoaanes@gmail.com>
2026-09-07 14:50:27 +0200

    bedre formatering

--- eksempel-2.php
+++ eksempel-2.php
@@ -2,7 +2,7 @@
 $number1 = $_POST["number1"];
 $number2 = $_POST["number2"];
 
-    echo "Tallene dine er " . $number1 . " og " . $number2 . "<br>";
+    echo "Tallene dine er " . $number1 . " og " . $number2 . "<br>" . "<br>";
     echo "Summen av " . $number1 . " og " . $number2 . " er " . ($number1 + $number2) . "<br>";
     echo "Differansen mellom " . $number1 . " og " . $number2 . " er " . ($number1 - $number2) . "<br>";
     echo "Produktet av " . $number1 . " og " . $number2 . " er " . ($number1 * $number2) . "<br>";
