ed957a7e50a8139b1be0704cd80f9cb1f6f13163
teoaanes <teoaanes@gmail.com>
2026-09-07 14:49:09 +0200

    ganging og deling

--- eksempel-2.php
+++ eksempel-2.php
@@ -5,5 +5,7 @@ $number2 = $_POST["number2"];
     echo "Tallene dine er " . $number1 . " og " . $number2 . "<br>";
     echo "Summen av " . $number1 . " og " . $number2 . " er " . ($number1 + $number2) . "<br>";
     echo "Differansen mellom " . $number1 . " og " . $number2 . " er " . ($number1 - $number2) . "<br>";
+    echo "Produktet av " . $number1 . " og " . $number2 . " er " . ($number1 * $number2) . "<br>";
+    echo "Kvotienten av " . $number1 . " og " . $number2 . " er " . ($number1 / $number2) . "<br>";
     echo "<button type='button' onclick='window.location.href=\"index.html\"'>Tilbake</button>";
 ?>
