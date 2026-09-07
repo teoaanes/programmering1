ff5334fae3f80a423c0507190caf50c21d814c8e
teoaanes <teoaanes@gmail.com>
2026-09-07 14:29:04 +0200

    lagt til alder og eksempel 2 med beregning

--- eksempel-1.php
+++ eksempel-1.php
@@ -3,5 +3,5 @@ $fornavn = $_POST["fornavn"];
 $etternavn = $_POST["etternavn"];
 $alder = $_POST["alder"];
 
-    echo "God dag " . $fornavn . " " . $etternavn . "!" . "<br>" . "<button type='button' onclick='window.location.href=\"index.html\"'>Tilbake</button>";
+    echo "God dag " . $fornavn . " " . $etternavn . "!" . "<br>" . "Du er " . $alder . " år gammel." . "<br>" . "<button type='button' onclick='window.location.href=\"index.html\"'>Tilbake</button>";
 ?>
