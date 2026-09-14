b9aa36b7511160708c5e383cf3859ada0497f830
teoaanes <teoaanes@gmail.com>
2026-09-14 13:23:58 +0200

    styling linket til skjemaside

--- oppgave2/script.php
+++ oppgave2/script.php
@@ -8,9 +8,11 @@ $number = $_POST["matte"];
 
 if ($number == 9) {
     echo "Korrekt, svaret er 9";
+     echo "<button type='button' id='tilbake' onclick='window.location.href=\"index.html\"'>Tilbake til Index</button>";
 }
 
 else {
     echo"Feil svar, " . $number . " er ikke riktig";
+     echo "<button type='button' id='tilbake' onclick='window.location.href=\"index.html\"'>Tilbake til Index</button>";
 }
 ?>
