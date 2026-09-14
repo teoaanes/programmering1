853703073526f8c2101003cc02a477a787550c6f
teoaanes <teoaanes@gmail.com>
2026-09-14 15:33:03 +0200

    eks 6 test

---
+++ eksempel-6/script.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" href="eksempel-6\style.css">
+    <title>Eksempel 6</title>
+    <meta charset="UTF-8">
+</head>
+
+<?php
+
+$tall1 =$_POST["tall1"];
+$tall2 =$_POST["tall2"];
+
+if($tall1<$tall2){
+    echo"Dine tall er $tall1 og $tall2" . "<br> $tall2 er større enn $tall1";
+}
+
+?>
