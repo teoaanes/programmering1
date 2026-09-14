1cd563ce3f5abe54c43ae8e102e459c0ac687109
teoaanes <teoaanes@gmail.com>
2026-09-14 13:31:04 +0200

    tilbakeknapper har faktisk fått riktig path

--- oppgave2/script.php
+++ oppgave2/script.php
@@ -9,12 +9,12 @@ $number = $_POST["matte"];
 if ($number == 9) {
     echo "Korrekt, svaret er 9";
     echo "<button type='button' id='tilbake' onclick='window.location.href=\"index.html\"'>Tilbake til Index</button>";
-    echo "<button type='button' id='tilbake' onclick='window.location.href=\oppgave2\"oppgave2.html\"'>Tilbake</button>";
+    echo "<button type='button' id='tilbake' onclick='window.location.href=\"oppgave2/oppgave2.html\"'>Tilbake</button>";
      }
 
 else {
     echo"Feil svar, " . $number . " er ikke riktig";
     echo "<button type='button' id='tilbake' onclick='window.location.href=\"index.html\"'>Tilbake til Index</button>";
-    echo "<button type='button' id='tilbake' onclick='window.location.href=\oppgave2\"oppgave2.html\"'>Tilbake</button>";
+    echo "<button type='button' id='tilbake' onclick='window.location.href=\"oppgave2/oppgave2.html\"'>Tilbake</button>";
 }
 ?>
