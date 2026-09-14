0bfe91b3acce1958cb00e8601ad6070c8be36726
teoaanes <teoaanes@gmail.com>
2026-09-14 13:13:35 +0200

    oppgave 2

---
+++ oppgave2/script.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+<?php
+
+$number = $_POST["matte"];
+
+if ($number == "!9") {
+    echo"Feil, $number . er ikke riktig";
+}
+
+else {
+    echo"Korrekt, svaret er 9";
+}
+?>
