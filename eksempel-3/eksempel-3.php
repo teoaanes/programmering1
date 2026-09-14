0c30dd6cace950756ab8a81c09356fc03231d6c8
teoaanes <teoaanes@gmail.com>
2026-09-14 16:15:59 +0200

    eks 3 få alt på samme side

--- eksempel-3/eksempel-3.php
+++ eksempel-3/eksempel-3.php
@@ -8,7 +8,7 @@
     <form action="" method="POST">
         <input type="number" name="matte" required>
         <button type="submit">Sjekk</button>
-        <button type="reset">Nullstill</button>
+        <button type="reset" name="nullstill">Nullstill</button>
     </form>
 
 <div><?php
