b7ec4d5fb49bdaf00dcb17ddaa1c70b3347566ad
teoaanes <teoaanes@gmail.com>
2026-09-14 14:33:42 +0200

    fikset svaralternativer med liten bokstav

--- eksempel-4/script.php
+++ eksempel-4/script.php
@@ -24,14 +24,14 @@ if (!$svar or !$alder) {
     }
 }
 
-else if ($svar == "Ja || ja" && $alder < 18) {
+else if ($svar == "Ja" || "ja" && $alder < 18) {
     echo "Du har svart ja, og at du er under 18 (OK Young Sheldon ahh)";
 }
-else if ($svar == "Ja || ja" && $alder) {
+else if ($svar == "Ja" || "ja" && $alder) {
     echo "Du har svart ja og at du er $alder";
 }
 
-else if ($svar == "Nei || nei" && $alder) {
+else if ($svar == "Nei" || "nei" && $alder) {
     echo "Du har svart nei og at du er $alder";
 }
 
