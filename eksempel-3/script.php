57b7494a84fdbd44ca06a8e293460029842c2182
teoaanes <teoaanes@gmail.com>
2026-09-14 13:49:16 +0200

    eksempel 4

--- eksempel-3/script.php
+++ eksempel-3/script.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <head>
-    <link rel="stylesheet" href="oppgave2/style.css">
+    <link rel="stylesheet" href="eksempel-3/style.css">
 </head>
 <?php
 
@@ -9,12 +9,12 @@ $number = $_POST["matte"];
 if ($number == 9) {
     echo "Korrekt, svaret er 9";
     echo "<button type='button' id='tilbake' onclick='window.location.href=\"index.html\"'>Tilbake til Index</button>";
-    echo "<button type='button' id='tilbake' onclick='window.location.href=\"oppgave2/oppgave2.html\"'>Tilbake</button>";
+    echo "<button type='button' id='tilbake' onclick='window.location.href=\"eksempel-3/eksempel-3.html\"'>Tilbake</button>";
      }
 
 else {
     echo"Feil svar, " . $number . " er ikke riktig";
     echo "<button type='button' id='tilbake' onclick='window.location.href=\"index.html\"'>Tilbake til Index</button>";
-    echo "<button type='button' id='tilbake' onclick='window.location.href=\"oppgave2/oppgave2.html\"'>Tilbake</button>";
+    echo "<button type='button' id='tilbake' onclick='window.location.href=\"eksempel-3/eksempel-3.html\"'>Tilbake</button>";
 }
 ?>
---
+++ eksempel-4/script.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+    <head>
+        <link rel="stylesheet" href="eksempel-4\style.css">
+    </head>
+
+<body>
+
+<?php
+
+$svar = $_POST["svar"];
+
+if ($svar == "!svar") {
+    echo"Du har ikke svart";
+}
+else if ($svar == "Ja") {
+    echo "Du har svart ja";
+}
+
+elseif ($svar == "Nei") {
+    echo "Du har svart nei";
+}
+
+else if ($svar == "!Ja or !Nei") {
+    echo "Du har ikke svart ja eller nei";
+}
+
+?>
+
+
+</body>
