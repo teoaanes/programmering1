ee1a99c00d6cc139d1a20d3ef2bfac642470f0f6
teoaanes <teoaanes@gmail.com>
2026-09-14 14:53:12 +0200

    eksempel 5 test

---
+++ eksempel-5/script.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" href="eksempel-5\style.css">
+</head>
+
+<?php
+
+$tall1=$_POST["tall1"];
+$tall2=$_POST["tall2"];
+$tall3=$_POST["tall3"];
+
+if($tall3== "1"){
+    echo"$tall1 + $tall2";
+}
+?>
