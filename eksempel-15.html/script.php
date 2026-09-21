a7ddebc30fec531f7c0cc3d5503a6da19e776b3f
teoaanes <teoaanes@gmail.com>
2026-09-21 17:02:06 +0200

    oppgave 15

--- eksempel-15.html/script.php
+++ eksempel-15.html/script.php
@@ -3,8 +3,9 @@
 $tall = $_POST['tall'];
 $tall2 = explode(",", $tall);
 
+echo"Tallene i rekkefølgen de er skrevet inn i: <br>";
 for ($i = 0; $i < count($tall2); $i++) {
-    echo $tall[$i] . "<br>";
+    echo $tall2[$i] . "<br>";
 }
 
 ?>
