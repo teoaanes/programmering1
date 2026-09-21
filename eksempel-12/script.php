47fd41652d97b71bf9bcacc95a36bd84200af570
teoaanes <teoaanes@gmail.com>
2026-09-21 16:09:09 +0200

    oppgave 12

--- eksempel-12/script.php
+++ eksempel-12/script.php
@@ -2,10 +2,11 @@
 
 $tall = 1;
 $matte = 0;
+$antall = 10;
 
 for ($tall = 1; $tall <= 10; $tall++) {
     $matte += $tall;
 }
- echo $matte . "<br>" . "gjennomsnittet av tallene 1-10 er: " . $matte/$tall;
+ echo $matte . "<br>" . "gjennomsnittet av tallene 1-10 er: " . $matte/$antall;
 
 ?>
