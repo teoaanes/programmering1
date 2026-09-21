b04f8887b7a65d8af0454e7e073fcd8d875863cb
teoaanes <teoaanes@gmail.com>
2026-09-21 15:40:47 +0200

    oppgave 10

---
+++ eksempel-10/script.php
@@ -0,0 +1,14 @@
+<?php
+
+$tall = 1;
+
+for ($tall = 1; $tall <= 10; $tall++) {
+    echo $tall . "<br>";
+}
+for ($tall = 11; $tall <= 20; $tall++) {
+    echo $tall . "<br>";
+}
+for ($tall = 21; $tall <= 30; $tall++) {
+    echo $tall . "<br>";
+}
+?>
